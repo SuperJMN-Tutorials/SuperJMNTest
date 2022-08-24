c7225cfe93f7345fa2b04dcadb6e015628463b37
José Manuel Nieto <superjmn@outlook.com>
2022-08-24 13:40:38 +0200

    Improvement

--- SuperJMNTest/ViewModels/MainWindowViewModel.cs
+++ SuperJMNTest/ViewModels/MainWindowViewModel.cs
@@ -1,106 +1,46 @@
-using ReactiveUI;
-using SuperJMNTest.Models;
 using System;
-using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace SuperJMNTest.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public IObservable<string> NombreCompletoTextBox { get; }
-        public IObservable<bool> IsButtonEnabled { get; }
-        public IObservable<bool> IsFormCompleted { get; }
-        //private string NombreCompletoTextBox { get; set; }
-        //private bool IsButtonEnabled { get; set; }
-        private bool _isFormCompletedField { get; set; }
-        public bool IsFormCompletedField
-        {
-            get => _isFormCompletedField;
-            set
-            {
-                _isFormCompletedField = value;
-                this.RaisePropertyChanged(nameof(IsFormCompletedField));
-            }
-        }
-
-        private string _nombreTextBox = "";
-        public string NombreTextBox
-        {
-            get => _nombreTextBox;
-            set
-            {
-                _nombreTextBox = value;
-                this.RaisePropertyChanged(nameof(NombreTextBox));
-            }
-        }
-        private string _apellidoTextBox = "";
-        public string ApellidoTextBox
-        {
-            get => _apellidoTextBox;
-            set
-            {
-                _apellidoTextBox = value;
-                this.RaisePropertyChanged(nameof(ApellidoTextBox));
-            }
-        }
-        private string _edadTextBox = "";
-        public string EdadTextBox
-        {
-            get => _edadTextBox;
-            set
-            {
-                _edadTextBox = value;
-                this.RaisePropertyChanged(nameof(EdadTextBox));
-            }
-        }
-
         public MainWindowViewModel()
         {
-            //this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox)
-            //.Subscribe(tuple =>
-            //{
-            //    var (nombreTextBox, apellidoTextBox) = tuple;
-            //    NombreCompletoTextBox = nombreTextBox + " " + apellidoTextBox;
-            //    Trace.WriteLine("test: " + NombreCompletoTextBox);
-            //});
+            NombreCompletoTextBox = this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox,
+                (a, b) => string.Join(" ", a, b));
 
-            //this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox, x => x.EdadTextBox)
-            //.Subscribe(tuple =>
-            //{
-            //    var (nombreTextBox, apellidoTextBox, edadTextBox) = tuple;
-            //    IsButtonEnabled = (nombreTextBox ?? "") != "" && (apellidoTextBox ?? "") != "" && (edadTextBox ?? "") != "";
-            //    Trace.WriteLine("test: " + IsButtonEnabled.ToString());
-            //});
+            IsValid = this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox, x => x.EdadTextBox,
+                (a, b, c) => IsFulfilled(a, b) && c >= 18);
+            
+            Submit = ReactiveCommand.Create(() => { /* the command itself does nothing */ }, IsValid);
 
-            NombreCompletoTextBox =
-            this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox)
-                .Select(tuple =>
-                {
-                    var (nombreTextBox, apellidoTextBox) = tuple;
-                    Trace.WriteLine("OK:" + tuple.ToString());
-                    return nombreTextBox + " " + apellidoTextBox;
-                });
-
-            IsButtonEnabled =
-            this.WhenAnyValue(x => x.NombreTextBox, x => x.ApellidoTextBox, x => x.EdadTextBox)
-                .Select(tuple =>
-                {
-                    var (nombreTextBox, apellidoTextBox, edadTextBox) = tuple;
-                    return (nombreTextBox ?? "") != "" && (apellidoTextBox ?? "") != "" && (edadTextBox ?? "") != "";
-                });
-
-            IsFormCompleted =
-            this.WhenAnyValue(x => x.IsFormCompletedField)
-                .Select(x =>
-                {
-                    return x;
-                });
+            IsSubmitted = Submit
+                .Any()
+                .StartWith(false);
         }
-        public void OnClickCommand()
+
+        private static bool IsFulfilled(params string[] strings)
         {
-            IsFormCompletedField = true;
+            return !strings.Any(string.IsNullOrEmpty);
         }
 
+        public IObservable<bool> IsSubmitted { get; }
+
+        public ReactiveCommand<Unit, Unit> Submit { get; }
+
+        public IObservable<string> NombreCompletoTextBox { get; }
+
+        public IObservable<bool> IsValid { get; }
+
+        [Reactive] public string NombreTextBox { get; set; }
+
+        [Reactive] public string ApellidoTextBox { get; set; }
+
+        [Reactive] public int EdadTextBox { get; set; }
     }
 }
